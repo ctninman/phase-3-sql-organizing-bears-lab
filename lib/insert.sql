599e3871a227e5fc74844a622cf12b6573d10475
ctninman <89664157+ctninman@users.noreply.github.com>
2022-01-13 00:14:32 +0100

    commit tree c48f3d2d2ec7dfb742741e5b3699039b9415e00d

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Purple", "HappyGoLucky", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "F", "Purple", "Angry", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Purple", "HappyGoLucky", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "Purple", "Bitey", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "M", "Purple", "Bitey", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "F", "Purple", "HappyGoLucky", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "M", "Purple", "Depressed", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, "M", "Purple", "Depress", 0);
